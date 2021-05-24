e9a9e0d0f3408447b655789bd8d25bb31ef21e4e
Ankita1498 <82357517+Ankita1498@users.noreply.github.com>
2021-05-24 23:27:04 +0530

    Add files via upload

---
+++ MVCDemoApp/MVCDemoApp/Controllers/AssignmentController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVCDemoApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVCDemoApp.Controllers
+{
+    public class AssignmentController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult PersonForm()
+        {
+            return View(new Person());
+        }
+
+        public IActionResult DisplayPersonData(Person person)
+        {
+            ViewBag.DefaultMessage = "Thank You " + person.Name;
+            if (person.WillAttend == "Yes")
+            {
+                ViewBag.Message = "Thank you for coming  ";
+            }
+            if (person.WillAttend == "No")
+            {
+                ViewBag.Message = "Sorry to hear you cant come,see you next time!  ";
+            }
+
+            return View();
+
+        }
+    }
+}
---
+++ MVCDemoApp/MVCDemoApp/Models/Person.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVCDemoApp.Models
+{
+    public class Person
+    {
+       
+
+        [Required(ErrorMessage = "Name is compulsory field")]
+        [StringLength(10, ErrorMessage = "Maximum Name length is 10 characters")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Email is compulsory field")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Phone is compulsory field")]
+        public int? Phone { get; set; }
+
+        [Required(ErrorMessage = "Select any one, it is a compulsory field")]
+        public string WillAttend { get; set; }
+    }
+}
